b9950ec7e1429d17326bea8f771c4827ebdf2bf8
Haven <leoshelleyyellow8@gmail.com>
2023-10-13 16:49:51 +0100

    task 2 worked on

--- Assessment 1/task-2/main.cpp
+++ Assessment 1/task-2/main.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+string name;
+string username;
+string clantag;
+int age;
+bool correctUsername = false;
+string confirmation;
+
+
 
 void main()
 {
@@ -16,5 +27,53 @@ void main()
     // - Username: [<clantag>]<username>
     // - Age: <age>
     //
+
+    cout << "Hello! what's your name? ";
+    getline(cin, name);
+
+    for (size_t i = 0; i < name.length(); i++)
+    {
+        if (name[i] == 0 || name[i - 1] == ' ') 
+        {
+            name[i] = toupper(name[i]);
+        }
+
+        else
+        {
+            name[i] = tolower(name[i]);
+        }
+    }
+
+
+    cout << "\nwhat a nice name! " << name << ", what's your username? ";
+
+    //correct username checker
+    while (correctUsername = true)
+    {
+        getline(cin, username);
+        cout << "\n" << name << ", that's a really good username but is " << username << " correct? (yes or no) ";
+        cin >> confirmation;
+        for (auto& c : confirmation){c = tolower(c);}
+        if (confirmation == "yea" || confirmation == "yes" || confirmation == "yeah" || confirmation == "yeag" || confirmation == "")
+        {
+            correctUsername = true;
+            break;
+        }
+        else 
+        {
+            cout << "\nthat's a shame, what is your correct username? "; 
+        }
+
+    }
+
+    cout << "\nnow, " << name << " enter the clantag that will go with your username (" << username << "). You are allowed up to 4 characters: ";
+    cin >> clantag;
+    clantag.resize(4);
+    for (size_t i = 0; i < clantag.length(); i++) clantag [i] = toupper(clantag[i]);
+    cout << "\n your clantag is: " << clantag;
+
+    cout << "\n" << name << ", your username is " << username << ", and your clantag is " << clantag << ". making the visible username: [" << clantag << "]" << username;
+
+
     
 }
